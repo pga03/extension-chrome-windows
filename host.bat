74edf145ff72ba1e9a9502ebe40db0338ad828e2
John Workmeister <jtworkme@us.ibm.com>
2016-01-15 13:00:51 -0500

    moving stuff around

--- host.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-python "C:\Users\IBM_ADMIN\projects\extension-chrome-windows\python-native-messaging-example-host" %*
--- install-host.bat
+++
@@ -1,1 +0,0 @@
-REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.ibm.firstdiscovery" /ve /t REG_SZ /d "C:\Users\IBM_ADMIN\projects\extension-chrome-windows\com.ibm.firstdiscovery.json" /f
