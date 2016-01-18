ace1211be938e54a5a2b31ac64f26722ad254a00
John Workmeister <jtworkme@us.ibm.com>
2016-01-18 09:29:11 -0500

    added connection checking in popup.js

--- popup.js
+++ popup.js
@@ -1,20 +1,26 @@
 /*It would be nice to inline this in popup.html but Chrome's security
   policy prohibits inline scripts in extensions*/
 
-//TODO: OPEN THE ERROR PAGE IF WE CAN'T CONNECT TO THE HOST.
+//TODO: This is somewhat kludgy. We shouldn't assume isConnectable is true,
+//      which is far too optimistic. We should assume isConnectable is false
+//      and then prove otherwise.
 
-//var isConnectable;
+var isConnectable = true;
 
-//var hostName, port = null;
-//hostName = "com.ibm.firstdiscovery";
-//console.log("Connecting to native messaging host: " + hostName);
-//port = chrome.runtime.connectNative(hostName);
-//port.onDisconnect.addListener(onDisconnected);
+var hostName, port = null;
+hostName = "com.ibm.firstdiscovery";
+console.log("Checking connection to native host: " + hostName);
+port = chrome.runtime.connectNative(hostName);
+port.onDisconnect.addListener(onDisconnected);
 
-//function onDisconnected() {
-//  console.log("Failed to connect: " + chrome.runtime.lastError.message);
-//  port = null;
-//  chrome.tabs.create({url : "chrome-extension://fojfkfdnipccdhhjakieojkglomfaloe/error.html"});
-//}
+function onDisconnected() {
+  console.log("Failed to connect: " + chrome.runtime.lastError.message);
+  port = null;
+  chrome.tabs.create({url : "chrome-extension://fojfkfdnipccdhhjakieojkglomfaloe/error.html"});
+  isConnectable = false;
+}
 
-chrome.tabs.create({url : "http://extension-demo-jtworkme.mybluemix.net/"});
+//After a check for the native host, we go to the demo site
+if (!(isConnectable === false || isConnectable === undefined || isConnectable === null)){
+  chrome.tabs.create({url : "http://extension-demo-jtworkme.mybluemix.net/"});
+}
