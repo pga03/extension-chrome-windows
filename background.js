3bd011066e9b1206a6b073338cc349b7344651a4
John Workmeister <jtworkme@us.ibm.com>
2016-01-19 11:38:01 -0500

    fixed background.js

--- background.js
+++ background.js
@@ -1,73 +1,29 @@
-/******************************************************************************
- *                               Native Messaging                              *
- ******************************************************************************/
-
-var port, message = null;
-var response;
-
-function connectToNativeHost() {
-  var hostName = "com.ibm.firstdiscovery";
-  console.log("Connecting to native messaging host: " + hostName);
-  port = chrome.runtime.connectNative(hostName);
-  port.onMessage.addListener(onNativeMessage);
-  port.onDisconnect.addListener(onDisconnected);
-}
-
-function sendNativeMessage(message) {
-  port.postMessage(message);
-  console.log("Sending to native host: "+ JSON.stringify(message));
-}
-
-function onNativeMessage(message) {
-  console.log("Message from native host: " + JSON.stringify(message));
-  if (message.is_successful === "true"){
-    console.log("Successful message!");
-  }
-  else {
-    console.log("Unsuccessful message! :(");
-  }
-  //The page needs refreshed to see the response
-  chrome.runtime.onMessageExternal.addListener(
-    function (request, sender, sendResponse) {
-      sendResponse(message);
-    });
-}
-
-function onDisconnected() {
-  console.log("Failed to connect: " + chrome.runtime.lastError.message);
-  port = null;
-}
-
-//Add the listener to the background page
-document.addEventListener('DOMContentLoaded', function () {
-  connectToNativeHost();
-});
-
-/******************************************************************************
- *                            Non-Native Messaging                             *
- ******************************************************************************/
+var hostname = "com.ibm.firstdiscovery";
 
 // Listen to events from our web app and handle them.
 chrome.runtime.onMessageExternal.addListener(webListener);
 
 function webListener(request, sender, sendResponse) {
-  //Parse message string into JSON object
+  try {
+    if (request && request.message) {
 
-  if (request.message){
-    var message = request.message;
-
-    console.log("Stringified Message: " + JSON.stringify(message));
-
-    //Pass the non-native message to the native host
-    var response = sendNativeMessage(message);
-
-    var msgResponse = {
-      "received": request
-    };
-    console.log("Request: " + JSON.stringify(msgResponse));
-
-  } else{
-    console.log("undefined request.message recieved");
+      if (request.message.message_type === "write_usb") {
+        chrome.runtime.sendNativeMessage(hostName, request.message,
+            function onNativeMessageResponse(response){
+              console.log("LINE 61: " + JSON.stringify(response));
+              sendResponse(response);
+            });
+      }
+      else if (request.message.message_type === "request_version") {
+        sendResponse({"extension":"1", "native": "unknown"});
+      }
+      else {
+        sendResponse({"error": true, "msg": "Unknown message type"});
+      }
+    }
+  } catch (ex) {
+    sendResponse({"error": true, "msg": "An exception occured", err: ex});
   }
-
+  // see http://stackoverflow.com/questions/20077487/chrome-extension-message-passing-response-not-sent
+  return true;
 }
