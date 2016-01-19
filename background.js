b9a57548ea7d5541437271b154a47ac4c291f195
John Workmeister <jtworkme@us.ibm.com>
2016-01-19 11:51:33 -0500

    adding logging

--- background.js
+++ background.js
@@ -7,11 +7,13 @@ chrome.runtime.onMessageExternal.addListener(webListener);
 function webListener(request, sender, sendResponse) {
   try {
     if (request && request.message) {
-      console.log("request.message: " + request.message);
+      console.log("request.message: " + JSON.stringify(request.message));
+
       if (request.message.message_type === "write_usb") {
         console.log("Got call to write to USB");
         chrome.runtime.sendNativeMessage(hostName, request.message,
             function onNativeMessageResponse(response){
+
               console.log("Response from Native: " + JSON.stringify(response));
               if (response.is_successful === "true"){
                 console.log("Successful message from native host");
@@ -19,16 +21,20 @@ function webListener(request, sender, sendResponse) {
                 console.log("Unsuccessful message from native host");
               }
               sendResponse(response);
+
             });
       }
+
       else if (request.message.message_type === "request_version") {
         sendResponse({"extension": extensionVersion, "native": "unknown"});
       }
       else {
+        console.log("Unknown message type");
         sendResponse({"error": true, "msg": "Unknown message type"});
       }
     }
   } catch (ex) {
+    console.log("Exception occured!");
     sendResponse({"error": true, "msg": "An exception occured", err: ex});
   }
   return true;
